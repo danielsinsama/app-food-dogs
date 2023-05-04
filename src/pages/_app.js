c23a46885639b5b97808d28b253083c744a4532d
Daniel Ramirez <danielsinsama@gmail.com>
2023-05-03 20:59:05 -0500

    feat(Context): Use the Global Context in the web

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
 import '@/styles/globals.css'
-
+import { DogsContextProvider } from '@/context/dogs'
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return <DogsContextProvider>
+    <Component {...pageProps} />
+  </DogsContextProvider>
+
 }
